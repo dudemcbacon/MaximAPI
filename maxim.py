4344ec61e84302358fd86cdbe77c998bf731f1cc
Brandon Burnett <gentoolicious@gmail.com>
2013-01-04 15:09:34 -0800

    Added test.py and expanded __init__
    Expanded __init__ so that it would check that it's getting passed the
    right kind of instance for reader. Also added test.py to use as a test
    script for the class.

--- maxim.py
+++ maxim.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python
+from smartcard.System import readers
+from smartcard.util import toHexString
 
 class MaximAPI:
-	def __init__(self):
-		self.poop = poop
+
+	_PASSTHRU = [0x00, 0x00, 0x00, 0x00]
+	
+	def __init__(self, reader, a=0xA0):
+		if reader.__doc__ != "PCSC reader class.":
+			raise TypeError("reader not PCSC reader class")
+		else:
+			self.reader = reader
+		self.a = a
+		
+	def GetVersion(self):
+		self.foo = bar
---
+++ test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+from smartcard.System import readers
+import maxim
+
+r = readers()
+max = maxim.MaximAPI("poop")
